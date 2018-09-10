baf0a5b1551530c3f1f312f7896d1d97df556024
jeffrimko <jeffrimko@gmail.com>
2018-09-09 23:22:55 -0400

    Added BasicIEnumerator example.

---
+++ BasicIEnumerator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace BasicIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new NumberEnumerator(1.23f, 4.56f, 7.89f);
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class NumberEnumerator : IEnumerable, IEnumerator
+    {
+        private float[] myNums;
+        private int index;
+
+        public NumberEnumerator(params float[] nums)
+        {
+            myNums = nums;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return (index < myNums.Length);
+        }
+
+        public object Current
+        {
+            get
+            {
+                return myNums[index];
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (MoveNext())
+            {
+                yield return Current;
+            }
+            Reset();
+        }
+    }
+}
---
+++ BasicIEnumerator/README.md
@@ -0,0 +1,3 @@
+# BasicIEnumerator
+
+Example basic implementation of [IEnumerator](https://docs.microsoft.com/en-us/dotnet/api/system.collections.ienumerator).
